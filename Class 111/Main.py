36a63a46c42fc8256aaf0ce0265fb6d13acb371b
Godspeed1983 <109015446+Godspeed1983@users.noreply.github.com>
2023-07-16 12:46:11 +0530

    Add files via upload

---
+++ Class 111/Main.py
@@ -0,0 +1,7 @@
+import os 
+import shutil
+
+source="C:/Users/Innoc/OneDrive/Desktop/Class 111/teamA/ChatBot-01.png"
+destinaiton="C:/Users/Innoc/OneDrive/Desktop/Class 111/teamB"
+
+a=shutil.move(source,destinaiton)
